f4bea354b1c84bf2dea420ff4902e331cada106a
Ethan <ethan.graham@epfl.ch>
2024-06-11 15:27:49 +0200

    updated main

--- server_protocol/main.go
+++ server_protocol/main.go
@@ -2,18 +2,23 @@
 package main
 
 import (
-	"fmt"
-    "github.com/ethangraham2001/distributed_kvs_server_protocol/peer"
+	"log"
+	"net/http"
+
+	"github.com/ethangraham2001/distributed_kvs_server_protocol/http_handlers"
+	"github.com/ethangraham2001/distributed_kvs_server_protocol/peer"
 )
 
 const filepath string = "config.yaml"
 
 func main() {
-    p, err := peer.ReadConfigFromFile[uint64, uint64](filepath)
+	p, err := peer.ReadConfigFromFile[string, []byte](filepath)
+	if err != nil {
+		panic("failed to initialize peer!")
+	}
 
-    if err != nil {
-        panic("failed to initialize peer!")
-    }
-
-    fmt.Printf("Current Peer-ID: %d\n", p.ID)
+    handlerFunc := httphandlers.InitHandleReq(&p)
+    http.HandleFunc(httphandlers.APIEndpoint, handlerFunc)
+    log.Print("Launching peer")
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
