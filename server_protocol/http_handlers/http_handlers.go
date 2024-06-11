509f81bf8f1d33b0111fd7bbb0d767931c581022
Ethan <ethan.graham@epfl.ch>
2024-06-11 15:26:51 +0200

    Added get and put handlers that read / write a raw byte array.

--- server_protocol/http_handlers/http_handlers.go
+++ server_protocol/http_handlers/http_handlers.go
@@ -1,33 +1,141 @@
+// Package httphandlers defines http handlers for client-to-peer communication
+// or peer-to-peer communication. Any business logic related to keys that
+// are contained in different Peers is not handled here
 package httphandlers
 
 import (
+	"encoding/json"
+	"io"
+	"log"
 	"net/http"
 
-	"github.com/ethangraham2001/distributed_kvs_server_protocol/server"
+	"github.com/ethangraham2001/distributed_kvs_server_protocol/peer"
 )
 
-var srv server.Server[int64, string]
+// APIEndpoint is the only available Endpoint for now.
+const APIEndpoint string = "/"
 
-type PutRequest struct {
-	key   int64
-	value string
+const contentType string = "Content-Type"
+const contentTypeOctetStream string = "application/octet-stream"
+const contentTypeJson string = "application/json"
+
+const maxBytes int = 10 << 20; // 10MiB limit
+ 
+// defines the put request body for json unmarshalling
+type putRequest[K comparable] struct {
+	Key K `json:"key"`
 }
 
-type GetRequest struct {
-	key int64
+// defines the get request body for json unmarshalling
+type getRequest[K comparable] struct {
+	Key K `json:"key"`
 }
 
-func HandleReq(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		handleGet(w, r)
-	case http.MethodPut:
-		handlePut(w, r)
+// InitHandleReq initializes a HandlerFunc that handles get and put requests
+// for a Peer p.
+// Get requests will return a raw byte array, which is why we contrain the
+// Peer.
+func InitHandleReq[K comparable](p *peer.Peer[K, []byte]) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			handleGet(w, r, p)
+		case http.MethodPut:
+			handlePut(w, r, p)
+		}
 	}
 }
 
-func handleGet(w http.ResponseWriter, r *http.Request) {
+func handleGet[K comparable](w http.ResponseWriter, r *http.Request, p *peer.Peer[K, []byte]) {
+    log.Print("Received GET request")
+	var decodedReq getRequest[K]
+
+	err := json.NewDecoder(r.Body).Decode(&decodedReq)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	key := decodedReq.Key
+
+	data, err := p.GetFromDatastore(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+	}
+
+	w.Header().Set(contentType, contentTypeOctetStream)
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
 }
 
-func handlePut(w http.ResponseWriter, r *http.Request) {
+func handlePut[K comparable](w http.ResponseWriter, r *http.Request, p *peer.Peer[K, []byte]) {
+    log.Print("Recived PUT request")
+	var decodedReq putRequest[K]
+
+	err := json.NewDecoder(r.Body).Decode(&decodedReq)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+    reader, err := r.MultipartReader()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    var key K
+    var data []byte
+
+    for {
+        part, err := reader.NextPart()
+
+        if err == io.EOF {
+            break;
+        }
+
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        if part.Header.Get(contentType) == contentTypeJson {
+            key, err = decodePutJson[K](r)
+            if err != nil {
+                http.Error(w, err.Error(), http.StatusBadRequest)
+                return
+            }
+        }
+
+        if part.Header.Get(contentType) == contentTypeOctetStream {
+            var data []byte
+            bytesRead, err := part.Read(data)
+            
+            if err != nil {
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+                return
+            }
+
+            if bytesRead == 0 {
+                http.Error(w, "Empty file", http.StatusBadRequest)
+                return
+            }
+        }
+    }
+
+    p.PutInDataStore(key, data)
 }
+
+// decodes put request json and returns the key
+func decodePutJson[K comparable](r *http.Request) (K, error) {
+    var decodedReq putRequest[K]
+    var key K
+
+    err := json.NewDecoder(r.Body).Decode(&decodedReq)
+	if err != nil {
+        return key, err
+	}
+
+    return key, nil
+}
+
